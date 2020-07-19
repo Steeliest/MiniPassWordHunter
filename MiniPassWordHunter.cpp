cd75542cf1e488174afacc2f55de597cf8680a5d
Steesha <2327488733@qq.com>
2020-07-19 14:20:18 +0800

    ©

--- MiniPassWordHunter.cpp
+++ MiniPassWordHunter.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-
+//==============================================================
+//  Copyright (C) 2020 Steeliest Org.. All rights reserved.
+//
+//  The information contained herein is confidential, proprietary
+//  to Jeteam Inc. Use of this information by anyone other than 
+//  authorized employees of Jeteam Inc is granted only under a 
+//  written non-disclosure agreement, expressly prescribing the 
+//  scope and manner of such use.
+//==============================================================
+//  Create by Steesha at 2020.7.19.
+//  Version 1.0
+//  Steesha [steesha@qq.com]
+//==============================================================
 int main()
 {
-    std::cout << "Hello World!\n";
+	return 0;
 }
